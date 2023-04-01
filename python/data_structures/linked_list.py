aeb375fcae66fc5632572f9a38f41b2083a289c7
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-04-01 16:44:43 -0500

    feat: created Link and Node classes. Performed initial tests.

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,33 @@
+class Node:
+    """
+    Node belonging to a LinkedList. Contains its own value and a reference
+    to the next node in the LinkedList.
+    """
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
 class LinkedList:
     """
-    Put docstring here
+    Structure containing nodes. This class contains only a reference to the
+    head (first) node.
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        old_head = self.head
+        self.head = Node(value, old_head)
+
+    def includes(self, value):
+        curr_node = self.head
+        while curr_node is not None:
+            if curr_node.value == value:
+                return True
+            curr_node = curr_node.next
+        return False
 
 
 class TargetError:
