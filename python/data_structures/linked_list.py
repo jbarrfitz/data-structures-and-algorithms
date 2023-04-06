1e91185650cb728960e54dc13c6791edb8c1ecb0
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-04-05 23:24:44 -0500

    feat: added append method and tests. All tests pass.

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -3,6 +3,7 @@ class Node:
     Node belonging to a LinkedList. Contains its own value and a reference
     to the next node in the LinkedList.
     """
+
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
@@ -25,6 +26,15 @@ class LinkedList:
             curr_node = curr_node.next
         return f"{node_strings}NULL"
 
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if not self.head:
+            self.head = new_node
+        else:
+            curr_node = self.head
+            while curr_node.next:
+                curr_node = curr_node.next
+            curr_node.next = new_node
 
     def insert(self, value):
         new_node = Node(value)
