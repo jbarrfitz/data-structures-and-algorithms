2cc4fa3afac7c835b6ffe24afd4ea5336f49d6ab
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-04-19 20:47:48 -0500

    feat: wrote Stack class and functions; provided tests pass

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,36 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Stack:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, top=None):
+        self.top = top
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        temp = self.top
+        self.top = Node(value, temp)
+
+    def pop(self):
+        if not self.top:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        popped = self.top
+        self.top = popped.next
+        return popped.value
+
+    def is_empty(self):
+        return not self.top
+
+    def peek(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
+
