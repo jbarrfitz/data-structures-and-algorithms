33144d8e0af83ac8b321c67f159233b44fe64d83
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-05-02 19:51:57 -0400

    feat: started implementation of AnimalShelter

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,58 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self, front=None, tail=None):
+        self.front = front
+        self.tail = tail
+
+    def enqueue(self, animal):
+        if not animal:
+            return
+        if animal.lower() == "dog":
+            new_animal = Dog()
+        elif animal.lower() == "cat":
+            new_animal = Cat()
+        else:
+            return
+        if not self.tail:
+            self.tail = self.front = new_animal
+        else:
+            self.tail.next = new_animal
+            self.tail = new_animal
+
+    def dequeue(self, pref=None):
+        if not self.front:
+            return
+        if pref == "dog":
+            match_class = "Dog"
+        elif pref == "cat":
+            match_class = "Cat"
+        else:
+            return
+        curr_animal = self.front.value
+        while True:
+            if isinstance(curr_animal, match_class):
+                dequeued = curr_animal
+
+        self.front = self.front.next
+        return dequeued
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError
+        return self.front.value
+
+    def is_empty(self):
+        return not self.front
 
 
 class Dog:
-    pass
+    def __init__(self, name="", next=None):
+        self.name = name
+        self.next = next
 
 
 class Cat:
-    pass
+    def __init__(self, name="", next=None):
+        self.name = name
+        self.next = next
