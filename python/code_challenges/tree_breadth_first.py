5399acaa5da19b691082d6f6dc8c295ed963d728
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-05-15 23:09:05 -0400

    feat: implemented code challenge and updated README files

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,17 @@
-from data_structures.binary_tree import BinaryTree
+from collections import deque
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    if not tree.root:
+        return []
+    queue = deque()
+    result = []
+    queue.append(tree.root)
+    while queue:
+        curr_node = queue.popleft()
+        result.append(curr_node.value)
+        if curr_node.left:
+            queue.append(curr_node.left)
+        if curr_node.right:
+            queue.append(curr_node.right)
+    return result
